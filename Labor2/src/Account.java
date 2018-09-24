d80856addcadc75303055fc568bc1e413d3cee82
locziagoston <43540084+locziagoston@users.noreply.github.com>
2018-09-24 17:46:44 +0200

    Add files via upload

--- Labor2/src/Account.java
+++ Labor2/src/Account.java
@@ -14,12 +14,76 @@ import java.util.Date;
 public class Account {
     private int id;
     private double balance;
-    private static double annualTR=10;
+    private static double annualTR = 10; //ez csak egyszer jön létre a memóriában, míg a többi töbször is
     private Date dateCreated;
     
+    public Account()
+    {
+            this.id = 0;
+            this.balance = 0;
+            this.dateCreated = new Date();
+            
+    }
+//alt+insert
+    public Account(int id, double balance, Date dateCreated) {
+        this.id = id;
+        this.balance = balance;
+        this.dateCreated = new Date();
+    }
+    
+    public int getId()
+    {
+        return this.id;
+    }
+    
+    public void setId(int v) //v az egy lokális változó a fügygvény belsejében bármi lehet
+    {
+        this.id=v;
+    }
+    
+    public double getBalance()
+    {
+        return this.balance;
+    }
+    
+    public void setBalance(double v)
+    {
+        this.balance=v;
+    }
+    
+    public Date getDateCreated()
+    {
+        return this.dateCreated;
+    }
+   
     public static double getAnnualTR()
     {
         return annualTR; //osztályszintű változó ezért nem kell this
+    }
+    
+    public static void setAnnualTR(double v)
+    {
+        annualTR=v;
+    }
+    
+    public static double getMounthlyTR()
+    {
+        return annualTR/12;
+    }
+    
+    public void withdraw(double pénz)
+    {
+        this.balance=this.balance-pénz;
+    }
+    
+    public void deposit(double pénz)
+    {
+        this.balance=this.balance+pénz;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" + "id=" + id + ", balance=" + balance + ", dateCreated=" + dateCreated + '}';
     }
     
 }
--- Labor2/src/main.java
+++ Labor2/src/main.java
@@ -9,6 +9,22 @@
  * @author hallgato
  */
 public class main {
+    
+    
+    
+    public static int lnko(int a, int b)
+    {
+        int r;
+        do{
+            r=a%b;
+            a=b;
+            b=r;
+            
+            
+        }while(r!=0);
+        return a;
+    }
+            
     public static void main(String[] args) {
 //        
 //        Rectangle r1= new Rectangle();
@@ -25,17 +41,40 @@ public class main {
 //        System.out.println(r2.getHeight()); //getterekkel setterekkel tudjuk manipulálni a private változókat
 //    }
 //}
-    QEquation e1 = new QEquation(1, 2, 3);
-    QEquation e2 = new QEquation(2, 4, 1);
-                 System.out.println("x1="+e1.getRoot1()+" "+e1.getRoot2());
-              System.out.println("x1="+e2.getRoot1()+" "+e2.getRoot2());
-              System.out.println(e1);
-    
-        Account ac;
+//    QEquation e1 = new QEquation(1, 2, 3);
+//    QEquation e2 = new QEquation(2, 4, 1);
+//                 System.out.println("x1="+e1.getRoot1()+" "+e1.getRoot2());
+//              System.out.println("x1="+e2.getRoot1()+" "+e2.getRoot2());
+//              System.out.println(e1);
+//    
+//        Account ac;
+//        
+//        System.out.println(Account.getAnnualTR()); //az osztály szintű adattagokra és metódusokra az osztály nevével és . operátorral hivatkozunk
+//        
+        /*Account ac1 = new Account(1, 10000);
+        System.out.println("Id: "+ac1.getId()+" Egyenleg: "+ac1.getBalance());
+        Account ac2 = new Account(2, 20000);
+        System.out.println("Id: "+ac2.getId()+" Egyenleg: "+ac2.getBalance());
+        Account ac3 = new Account(3, 30000);
+        System.out.println("Id: "+ac3.getId()+" Egyenleg: "+ac3.getBalance());
         
-        System.out.println(Account.getAnnualTR()); //az osztály szintű adattagokra és metódusokra az osztály nevével és . operátorral hivatkozunk
         
+        ac1.deposit(2000);
+        System.out.println("Id: "+ac1.getId()+" Egyenleg: "+ac1.getBalance());
         
+        ac1.withdraw(10000);
+        System.out.println("Id: "+ac1.getId()+" Egyenleg: "+ac1.getBalance());
+        */
+        final double pi=3.14;
         
+        System.out.println(Fan.MEDIUM);
+        Fan f = new Fan();
+        System.out.println(f);
+        f.setOn(true);
+        f.setSpeed(Fan.FAST);
+        System.out.println(f);
+        
+        System.out.println(Math.PI);
+        System.out.println(lnko(360,225));
     }
 }
